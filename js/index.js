6ffee78ed22adb64138e1258316d218247b23c74
andrew <neon.duncan@gmail.com>
2021-07-01 21:13:03 -0500

    completed assignment with OOP

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {});
+let currentUser
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  Api.fetchBooks()
+  getCurrentUser(1)
+});
+
+const getCurrentUser = (id) => {
+  fetch(" http://localhost:3000/users/" + id)
+    .then(resp => resp.json())
+    .then(user => {
+      currentUser = new User(user.id, user.username)
+      return currentUser
+  })
+}
